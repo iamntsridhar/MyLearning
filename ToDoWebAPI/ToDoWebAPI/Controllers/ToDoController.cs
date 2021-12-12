db4cae62bc210919dbd5ba622bf80954307f567e
whateveriknow <whateveriknow1983@gmail.com>
2021-12-12 12:56:47 +0530

    new chages

---
+++ ToDoWebAPI/ToDoWebAPI/Controllers/ToDoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoWebAPI.Data;
+
+namespace ToDoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ApiDBContext _dBContext;
+
+        public ToDoController(ApiDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+
+        [HttpGet]
+        [Route("getallitems")]
+        public async Task<IActionResult> GetAllItems()
+        {
+            var Items = await _dBContext.Items.ToListAsync();
+            return Ok(Items);
+        }
+
+        [HttpPost]
+        [Route("saveitem")]
+        public async Task<IActionResult> GetAllItems()
+        {
+            var Items = await _dBContext.Items.ToListAsync();
+            return Ok(Items);
+        }
+    }
+}
---
+++ ToDoWebAPI/ToDoWebAPI/Data/ApiDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoWebAPI.Models;
+
+namespace ToDoWebAPI.Data
+{
+    public class ApiDBContext : DbContext
+    {
+        public virtual DbSet<ItemData> Items { get; set; }
+
+        public ApiDBContext(DbContextOptions<ApiDBContext> Options) : base(Options)
+        {
+
+        }
+    }
+}
